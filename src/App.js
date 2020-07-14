4e7fdd5d6b6ccf8c8ecc9fc99d3cfb5791da9382
diego <diego@getdayone.com>
2020-07-14 11:41:02 +0200

    last commit m6-d7

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import Navigation from './components/Navigation';
 import BookList from "./components/BookList"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Cart from "./components/Cart"
+import { Button, Row } from 'react-bootstrap';
+import { DashCircleFill, PlusCircleFill } from "react-bootstrap-icons"
+
 
 
 class App extends React.Component {
 
   state = {
     books: [],
-    cart: []
+    cart: [],
+    page: 0,
+    pageSize: 12
+  }
+
+  setPage = async (page) => {
+    this.setState({
+      page: page
+    }, async () => {
+      await this.fetchData()
+    })
   }
 
   render() {
@@ -21,6 +34,10 @@ class App extends React.Component {
 
         <Switch>
           <Route path="/" exact >
+            <Row className="my-4 justify-content-center">
+              {this.state.page > 0 && <Button variant="success" onClick={() => this.setPage(this.state.page-1)}><DashCircleFill /></Button>}
+              <Button variant="success"  onClick={() => this.setPage(this.state.page + 1)}><PlusCircleFill /></Button>
+            </Row>
             <BookList books={this.state.books} />
           </Route>
           <Route path="/cart" exact >
@@ -31,12 +48,16 @@ class App extends React.Component {
     );
   }
 
+  fetchData = async () => {
+    const res = await fetch(`http://localhost:3456/books?limit=${this.state.pageSize}&offset=${this.state.page * this.state.pageSize}`)
+      const books = await res.json()
+      this.setState({
+        books: books
+      })
+  }
+
   componentDidMount = async () => {
-    const res = await fetch("https://striveschool-api.herokuapp.com/books")
-    const books = await res.json()
-    this.setState({
-      books: books
-    })
+    await this.fetchData()
 
     // const cartRes = await fetch("https://striveschool-api.herokuapp.com/books")
     // const cart = await res.json()
